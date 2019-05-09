bba0e7e5a298dd7f032569c9da3c8aa0ad8a707c
Dom <dominickirkham@gmail.com>
2019-05-09 10:35:51 +0100

    Added provided files

---
+++ gg2_python/back_project.py
@@ -0,0 +1,44 @@
+import numpy as np
+import math
+import scipy
+from scipy import interpolate
+import sys
+
+def back_project(sinogram, skip=1):
+
+	"""back_project back-projection to reconstruct CT data
+	back_project(sinogram) back-projects the filtered sinogram
+	(angles x samples) to create the reconstruted data (samples x
+	samples)"""
+
+	# get input dimensions
+	ns = sinogram.shape[1]
+	angles = sinogram.shape[0]
+	n = int(math.floor((ns-1) // skip) + 1)
+
+	# zero output and form input coordinates
+	# these have centre in the middle of the image
+	reconstruction = np.zeros((n, n))
+	xi, yi = np.meshgrid(np.arange(0,ns,skip) - (ns/2), np.arange(0,ns,skip) - (ns/2))
+
+	# back project over each angle in turn
+	for angle in range(angles):
+		sys.stdout.write("Reconstructing angle: %d   \r" % (angle + 1) )
+		
+		# Form rotated coordinates for output interpolation
+		# the rotation is about the middle of the image,
+		# but the output coordinates need to be relative to the top left
+		p = math.pi / 2 + angle * math.pi / angles
+		x0 = xi * math.cos(p) - yi * math.sin(p) + (ns / 2)
+		
+		# interpolate and add this data to output
+		# remembering to multiply by dtheta as well as sum
+		x2 = scipy.interpolate.interp1d(np.arange(0, ns, 1), sinogram[angle], kind='linear', copy=False, assume_sorted=True, bounds_error=False, fill_value=0, axis=0)
+		reconstruction = reconstruction + x2(x0) * (math.pi / angles)
+
+	# ensure any data outside the reconstructed circle is set to invalid
+	reconstruction[np.where((xi ** 2 + yi ** 2) > (ns/2)**2)] = -1
+
+	sys.stdout.write("\n")
+
+	return reconstruction
---
+++ gg2_python/ramp_filter.py
@@ -0,0 +1,24 @@
+import math
+import numpy as np
+
+def ramp_filter(sinogram, scale, alpha=0.001):
+	""" Ram-Lak filter with raised-cosine for CT reconstruction
+
+	fs = ramp_filter(sinogram, scale) filters the input in sinogram (angles x samples)
+	using a Ram-Lak filter.
+
+	fs = ramp_filter(sinogram, scale, alpha) can be used to modify the Ram-Lak filter by a
+	cosine raised to the power given by alpha."""
+
+	# get input dimensions
+	angles = sinogram.shape[0]
+	n = sinogram.shape[1]
+
+	#Set up filter to be at least twice as long as input
+	m = np.floor(np.log(n) / np.log(2) + 2)
+	m = int(2 ** m)
+
+	# apply filter to all angles
+	print('Ramp filtering')
+	
+	return sinogram
