6888053ad31055ded01c7de496a3b128e7469331
Bruno Mlodozeniec <bkmlodozeniec@gmail.com>
2019-05-20 11:33:38 +0100

    Add Graham test

---
+++ tests/manual_ct_test.py
@@ -0,0 +1,26 @@
+from material_data import *
+from photonsource_data import *
+from fake_source import *
+from ct_phantom import *
+from ct_lib import *
+from scan_and_reconstruct import *
+from create_dicom import *
+import numpy as np
+
+#create object instances
+material = MaterialData()
+source = PhotonSourceData()
+
+
+def test_flat():
+	p = ct_phantom(material.names, 256, 1)
+	s = fake_source(material.mev, 0.1, method='ideal')
+	y = scan_and_reconstruct(s, material, p, 0.01, 256)
+	plot(np.transpose([y[128, :], 0.207 * np.ones([256])]))
+	s = source.photons[5]
+	y = scan_and_reconstruct(s, material, p, 0.05, 256)
+	plot(np.transpose([y[128, :], 0.365 * np.ones([256])]))
+	y = scan_and_reconstruct(s, material, p, 0.5, 256)
+	draw(y, caxis=[0, 0.365])
+
+test_flat()
